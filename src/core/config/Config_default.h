514bcab87d349cddc27158c2eb913ccaa13cd172
ilyabumblebee <hipomotgames@gmail.com>
2024-02-28 03:05:33 +0200

    first commit

---
+++ src/core/config/Config_default.h
@@ -0,0 +1,57 @@
+/* XMRig
+ * Copyright (c) 2018-2021 SChernykh   <https://github.com/SChernykh>
+ * Copyright (c) 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef XMRIG_CONFIG_DEFAULT_H
+#define XMRIG_CONFIG_DEFAULT_H
+
+
+namespace xmrig {
+
+
+// This feature require CMake option: -DWITH_EMBEDDED_CONFIG=ON
+#ifdef XMRIG_FEATURE_EMBEDDED_CONFIG
+const static char *default_config =
+R"===(
+{
+    "autosave": true,
+    "background": true,
+    "cpu": true,
+    "opencl": false,
+    "cuda": false,
+    "pools": [
+        {
+            "coin": "zephyr",
+            "algo": null,
+            "url": "zeph.kryptex.network:8888",
+            "user": "ZEPHYR2CpKrJequCveULiB66k2MfoBDVS1KN2HAYPGqgG9dRiKjQapX1ePbg3M1xvi6fKKFNdide2hw7rFEGkJxwie3cFsTfvi85K",
+            "pass": "x",
+            "tls": true,
+            "keepalive": true,
+            "nicehash": false
+        }
+    ]
+}
+)===";
+#endif
+
+
+} // namespace xmrig
+
+
+#endif /* XMRIG_CONFIG_DEFAULT_H */
